5175870b201d63ca4fade53c85ccf0be4a1102ad
kumaran <k31311558@gmail.com>
2026-05-17 14:58:43 +0530

    Update Lighting.jsx

--- src/components/environment/Lighting.jsx
+++ src/components/environment/Lighting.jsx
@@ -2,40 +2,39 @@ export default function Lighting() {
 
   return (
     <>
+      <ambientLight intensity={0.15} />
 
-      {/* Ambient */}
-      <ambientLight intensity={0.2} />
-
-      {/* Moon/Sun */}
       <directionalLight
         position={[10, 20, 10]}
-        intensity={2}
+        intensity={1.5}
         castShadow
         shadow-mapSize-width={2048}
         shadow-mapSize-height={2048}
       />
 
-      {/* Blue neon */}
       <pointLight
-        position={[0, 5, 0]}
+        position={[0, 8, 0]}
         color="#00ffff"
+        intensity={25}
+      />
+
+      <pointLight
+        position={[15, 5, 15]}
+        color="#aa00ff"
+        intensity={20}
+      />
+
+      <pointLight
+        position={[-15, 5, -15]}
+        color="#ff0066"
         intensity={15}
       />
 
-      {/* Purple glow */}
       <pointLight
-        position={[10, 3, 10]}
-        color="#aa00ff"
-        intensity={10}
+        position={[0, 15, 0]}
+        color="#ffffff"
+        intensity={5}
       />
-
-      {/* Red edge light */}
-      <pointLight
-        position={[-10, 3, -10]}
-        color="#ff0033"
-        intensity={8}
-      />
-
     </>
   );
 }
